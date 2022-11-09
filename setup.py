f460a87522a590628993d8d553e0cf00a4ea8fac
Wes Hardaker <opensource@hardakers.net>
2022-11-09 08:10:30 -0800

    versionbump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aranet4-graph",
-    version="0.2",
+    version="0.3",
     author="Wes Hardaker",
     author_email="opensource@hardakers.net",
     description="Graph data file(s) from an aranet4 sensor.",
